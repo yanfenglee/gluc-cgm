2d6660ff69d2cbf9be62207d0eb090513dd9deb0
liyanfeng <muxsdt@gmail.com>
2022-02-07 17:23:30 +0800

    update test

--- tests/common.rs
+++
@@ -1,8 +0,0 @@
-use gluc_cgm::application;
-
-
-
-pub async fn setup() -> Result<(), anyhow::Error>{
-    application::run().await?;
-    Ok(())
-}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,8 +1,37 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test3333() {
-        assert!(false, "pass");
-        println!("asdf");
-    }
+use serde_json::json;
+
+//#[tokio::test]
+async fn register() -> Result<(), Box<dyn std::error::Error>> {
+    let echo_json: serde_json::Value = reqwest::Client::new()
+        .post("http://localhost:8899/user/register")
+        .json(&json!({
+            "username": "testname",
+            "password": "1234567",
+        }))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    assert!(echo_json["code"] == json!("OK"), "{}", echo_json);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn login() -> Result<(), Box<dyn std::error::Error>> {
+    let echo_json: serde_json::Value = reqwest::Client::new()
+        .post("http://localhost:8899/user/login")
+        .json(&json!({
+            "username": "testname",
+            "password": "1234567",
+        }))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    assert!(echo_json["code"] == json!("OK"), "{}", echo_json);
+
+    Ok(())
 }
