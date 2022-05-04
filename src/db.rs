534bc561aa8d6392f111aeeaf411069771ef9e2c
liyanfeng <muxsdt@gmail.com>
2022-05-05 00:56:04 +0800

    update

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,8 @@
 
-use mongodb::{Database, options::ClientOptions, Client, Collection};
+use mongodb::{Database, options::ClientOptions, Client, Collection, bson::doc, IndexModel};
 use once_cell::sync::OnceCell;
 
-use crate::settings::Settings;
+use crate::{settings::Settings, structs::{Cgm, User}};
 
 // mongodb singleton
 static INSTANCE: OnceCell<Database> = OnceCell::new();
@@ -23,6 +23,27 @@ impl DB {
         // Get a handle to the deployment.
         let db = Client::with_options(client_options)?.database(&Settings::get().database_name);
         let _ = INSTANCE.set(db);
+
+        Self::create_indexes().await?;
+
+        Ok(())
+    }
+
+    async fn create_indexes() -> Result<(), anyhow::Error> {
+        // init cgm index
+        let cgm_idx = IndexModel::builder()
+            .keys( doc!{"date": -1, "user_id": 1})
+            .options(None).build();
+        
+        Self::coll::<Cgm>().create_index(cgm_idx, None).await?;
+        
+        // init user index
+        let user_idx = IndexModel::builder()
+            .keys( doc!{"username": 1, "password": 1})
+            .options(None).build();
+
+        Self::coll::<User>().create_index(user_idx, None).await?;
+
         Ok(())
     }
 
--- tests/testall.py
+++ tests/testall.py
@@ -6,19 +6,20 @@ ctx = {}
 ctx['host'] = 'http://localhost:8899'
 
 ctx['username'] = f'testname{np.random.randint(0,10000000, 1)}'
+ctx['pswd'] = 'asdf'
 ctx['postdata'] = [{'device': 'xDrip-LimiTTer','date': 1608046900101,'dateString': '2020-12-15T23:41:40.100+0800','sgv': 162,'delta': -0.353,'direction': 'Flat','type': 'sgv','filtered': 181647.0452,'unfiltered': 181647.0452,'rssi': 100,'noise': 1,'sysTime': '2020-12-15T23:41:40.100+0800','utcOffset': None,'slope': None,'intercept': None,'scale': None,'mbg': None,'created_time': None}]
 
 tests = [
-    T(url='/user/register',method='post',data={'username':'$.username','password':'123456'}, expect={'code':'0'}),
-    T(url='/user/login',method='post',data={'username':'$.username','password':'123456'}, expect={'code':'0'},ctx={'login_token':'$.data.token'}),
+    T(url='/user/register',method='post',data={'username':'$.username','password':'$.pswd'}, expect={'code':'0'}),
+    T(url='/user/login',method='post',data={'username':'$.username','password':'$.pswd'}, expect={'code':'0'},ctx={'login_token':'$.data.token'}),
     
-    [
-        T(url='/api/v1/entries',method='post',headers={'token':'$.login_token'},data='$.postdata', expect={'code':'0'}),
-        T(url='/api/v1/entries.json',params={'rr':9999999999999999,'count':1},method='get',headers={'token':'$.login_token'},expect={'code':'0'}),
+    # [
+    #     T(url='/api/v1/entries',method='post',headers={'token':'$.login_token'},data='$.postdata', expect={'code':'0'}),
+    #     T(url='/api/v1/entries.json',params={'rr':9999999999999999,'count':1},method='get',headers={'token':'$.login_token'},expect={'code':'0'}),
 
-        T(url='/api/v1/entries',method='post',headers={'api-secret':'$.login_token'},data='$.postdata', expect={'code':'0'}),
-        T(url='/api/v1/entries.json',params={'rr':9999999999999999,'count':1},method='get',headers={'api-secret':'$.login_token'},expect={'code':'0'}),
-    ]*100
+    #     T(url='/api/v1/entries',method='post',headers={'api-secret':'$.login_token'},data='$.postdata', expect={'code':'0'}),
+    #     T(url='/api/v1/entries.json',params={'rr':9999999999999999,'count':1},method='get',headers={'api-secret':'$.login_token'},expect={'code':'0'}),
+    # ]
 
 ]
 
