ed5143214c5e0e4c39b3c3bacc57e884bf0c346b
liyanfeng <muxsdt@gmail.com>
2022-05-05 14:24:25 +0800

    unique key username

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,5 @@
 
-use mongodb::{Database, options::ClientOptions, Client, Collection, bson::doc, IndexModel};
+use mongodb::{Database, options::{ClientOptions, IndexOptions}, Client, Collection, bson::doc, IndexModel};
 use once_cell::sync::OnceCell;
 
 use crate::{settings::Settings, structs::{Cgm, User}};
@@ -32,17 +32,26 @@ impl DB {
     async fn create_indexes() -> Result<(), anyhow::Error> {
         // init cgm index
         let cgm_idx = IndexModel::builder()
-            .keys( doc!{"date": -1, "user_id": 1})
+            .keys( doc!{"date": -1i32, "user_id": 1i32})
             .options(None).build();
         
         Self::coll::<Cgm>().create_index(cgm_idx, None).await?;
         
         // init user index
         let user_idx = IndexModel::builder()
-            .keys( doc!{"username": 1, "password": 1})
+            .keys( doc!{"username": 1i32, "password": 1i32})
             .options(None).build();
 
         Self::coll::<User>().create_index(user_idx, None).await?;
+
+        // username unique index
+        let mut options = IndexOptions::default();
+        options.unique = Some(true);
+        
+        let username_idx = IndexModel::builder()
+            .keys( doc!{"username": 1i32})
+            .options(Some(options)).build();
+        Self::coll::<User>().create_index(username_idx, None).await?;
 
         Ok(())
     }
